f7424b33029ff283e1a67c6fc77d9f20bd574c8a
cnsdato <d.toplak@cadenas.de>
2012-07-27 11:41:47 +0200

    fix: build android native

--- source/android/imageResize.java
+++ source/android/imageResize.java
@@ -20,44 +20,44 @@ class imageResize
 {
     public boolean resizeImage(String src, String dest, int maxWidth, int maxHeight)
     {	
-		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-		bmOptions.inJustDecodeBounds = true;
-		BitmapFactory.decodeFile(src, bmOptions);
-		int imgWidth = bmOptions.outWidth;
-		int imgHeight = bmOptions.outHeight;
-		
-		int scaleFactor = 1;
-		if(imgHeight > imgWidth)
-		{
-			if(imgHeight > maxHeight)
-			{
-				scaleFactor = imgHeight / maxHeight;
-			}
-		}
-		else
-		{
-			if(imgWidth > maxWidth)
-			{
-				scaleFactor = imgWidth / maxWidth;
-			}
-		}
-		
-		/* Set bitmap options to scale the image decode target */
-		bmOptions.inJustDecodeBounds = false;
-		bmOptions.inSampleSize = scaleFactor;
-		bmOptions.inPurgeable = true;
-		
-		/* Decode the JPEG file into a Bitmap */
-		Bitmap bitmap = BitmapFactory.decodeFile(src, bmOptions);
-		
-		try{
-			FileOutputStream out = new FileOutputStream(dest);
-			bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		return true;
+      BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+      bmOptions.inJustDecodeBounds = true;
+      BitmapFactory.decodeFile(src, bmOptions);
+      int imgWidth = bmOptions.outWidth;
+      int imgHeight = bmOptions.outHeight;
+      
+      int scaleFactor = 1;
+      if(imgHeight > imgWidth)
+      {
+        if(imgHeight > maxHeight)
+        {
+          scaleFactor = imgHeight / maxHeight;
+        }
+      }
+      else
+      {
+        if(imgWidth > maxWidth)
+        {
+          scaleFactor = imgWidth / maxWidth;
+        }
+      }
+      
+      /* Set bitmap options to scale the image decode target */
+      bmOptions.inJustDecodeBounds = false;
+      bmOptions.inSampleSize = scaleFactor;
+      bmOptions.inPurgeable = true;
+      
+      /* Decode the JPEG file into a Bitmap */
+      Bitmap bitmap = BitmapFactory.decodeFile(src, bmOptions);
+      
+      try{
+        FileOutputStream out = new FileOutputStream(dest);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+      }
+      catch(Exception e)
+      {
+        e.printStackTrace();
+      }
+      return true;
     }
 }
